dfc26cfb33cd0641993ffbbcfcfd3e5a9f3b889c
Unknown <musabgamal.elkour@students.bfh.ch>
2019-04-01 09:02:18 +0200

    tracking events for Entspannunsübungen view

--- src/pages/menu_entspannungsUebungen/menu_entspannungsUebungen.ts
+++ src/pages/menu_entspannungsUebungen/menu_entspannungsUebungen.ts
@@ -1,44 +1,71 @@
  import {
-  Component
-} from '@angular/core';
-import {
-  NavController
-} from 'ionic-angular';
+   Component
+ } from '@angular/core';
+ import {
+   NavController
+ } from 'ionic-angular';
+ import {
+   MidataService
+ } from '../../services/midataService';
+ import {
+   MatomoTracker
+ } from 'ngx-matomo';
 
-@Component({
-  selector: 'page-entspannungsUebungen',
-  templateUrl: 'menu_entspannungsUebungen.html'
-})
-export class EntspannungsUebungen {
+ @Component({
+   selector: 'page-entspannungsUebungen',
+   templateUrl: 'menu_entspannungsUebungen.html'
+ })
+ export class EntspannungsUebungen {
 
-  selectedCard: Boolean = false;
-  selectedCard2: Boolean = false;
-  selectedCard3: Boolean = false;
+   selectedCard: Boolean = false;
+   selectedCard2: Boolean = false;
+   selectedCard3: Boolean = false;
 
-  constructor(public navCtrl: NavController) {}
+   private midataService: MidataService;
 
-  showText() {
-    if (this.selectedCard == false) {
-      this.selectedCard = true;
-    } else {
-      this.selectedCard = false;
-    }
-  }
+   constructor(public navCtrl: NavController, midataService: MidataService, private matomoTracker: MatomoTracker) {
+     this.midataService = midataService;
+   }
 
-  showText2() {
-    if (this.selectedCard2 == false) {
-      this.selectedCard2 = true;
-    } else {
-      this.selectedCard2 = false;
-    }
-  }
+   ngOnInit() {
+     //set user ID and document title 
+     if (this.midataService.loggedIn()) {
+       this.matomoTracker.setUserId(this.midataService.getUser().email);
+       this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking');
 
-  showText3() {
-    if (this.selectedCard3 == false) {
-      this.selectedCard3 = true;
-    } else {
-      this.selectedCard3 = false;
-    }
-  }
+       console.log(this.matomoTracker.setUserId(this.midataService.getUser().email))
+       console.log(this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking'))
+     } else {
+       this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking');
+     }
+     //Tracking Page view 
+     this.matomoTracker.trackPageView("Entspannungsübungen View besucht");
+   }
 
-}
+   showText() {
+     if (this.selectedCard == false) {
+       this.selectedCard = true;
+       //tracking event 
+       this.matomoTracker.trackEvent("Page: Entspannungsübungen", "Muskelrelaxation Übung klick")
+     } else {
+       this.selectedCard = false;
+     }
+   }
+
+   showText2() {
+     if (this.selectedCard2 == false) {
+       this.selectedCard2 = true;
+     } else {
+       this.selectedCard2 = false;
+     }
+   }
+
+   showText3() {
+     if (this.selectedCard3 == false) {
+       this.selectedCard3 = true;
+     } else {
+       this.selectedCard3 = false;
+     }
+   }
+
+ }
