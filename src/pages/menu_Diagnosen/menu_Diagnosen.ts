81e1b73de2993006ba75215dba9d0fc8032f3613
Unknown <musabgamal.elkour@students.bfh.ch>
2019-04-01 08:53:38 +0200

    tracking event for diagnose view

--- src/pages/menu_Diagnosen/menu_Diagnosen.ts
+++ src/pages/menu_Diagnosen/menu_Diagnosen.ts
@@ -17,6 +17,7 @@ import {
 import {
   LoginPage
 } from '../login/login';
+import { MatomoTracker } from 'ngx-matomo';
 
 @Component({
   selector: 'page-menu_Diagnosen',
@@ -32,8 +33,23 @@ export class Diagnosen {
 
   private midataService: MidataService;
 
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController, midataService: MidataService) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, midataService: MidataService, private matomoTracker: MatomoTracker) {
     this.midataService = midataService;
+  }
+
+  ngOnInit() {
+    //set user ID and document title 
+    if (this.midataService.loggedIn()) {
+      this.matomoTracker.setUserId(this.midataService.getUser().email);
+      this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking');
+
+      console.log(this.matomoTracker.setUserId(this.midataService.getUser().email))
+      console.log(this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking'))
+    } else {
+      this.matomoTracker.setDocumentTitle('Bachelorthesis START Tracking');
+    }
+    //Tracking Page view 
+    this.matomoTracker.trackPageView("Diagnosen View besucht");
   }
 
   onChangeDiagnoses() {
@@ -41,7 +57,12 @@ export class Diagnosen {
   }
 
   presentAlert() {
+    //tracking event 
+    this.matomoTracker.trackEvent("Page: Diagnosen", "Save Button klick")
+
     if (this.midataService.loggedIn()) {
+      //tracking event 
+      this.matomoTracker.trackEvent("Page: Diagnosen", "Save success")
 
       let alert = this.alertCtrl.create({
         message: 'Deine Diagnose wurde gespeichert',
