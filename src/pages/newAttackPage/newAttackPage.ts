b52b12fe3f39a319ffc319358a34fccf368d86c7
Unknown <musabgamal.elkour@students.bfh.ch>
2019-04-08 11:52:30 +0200

    persist handling pain period

--- src/pages/newAttackPage/newAttackPage.ts
+++ src/pages/newAttackPage/newAttackPage.ts
@@ -464,6 +464,13 @@ export class NewAttackPage {
       }).present()
     }
 
+    else if (this.selectedCard2 == false && this.symptome != null) {
+      this.alertCtrl.create({
+        message: "Bitte erfasse deine Schmerzdauer",
+        buttons: ['OK']
+      }).present()
+    }
+
     else if (this.group.get('symptome').hasError('required') || this.symptome == null) {
       console.log("Error: Selektiere minimum eine Auffälligkeit")
       return this.alertCtrl.create({
